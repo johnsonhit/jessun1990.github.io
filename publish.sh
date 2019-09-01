85eed72a7dc42cc8c21514995041d25963650bac
jessun <jessun1990@gmail.com>
2019-09-02 02:23:08 +0800

    gen site

--- publish.sh
+++ publish.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 git submodule update
 hugo
-git add public/
+git add .
 git cm -m 'gen site'
-git co master
-rm -r ./*
-git co src -- public
-mv public/* .
-rm -r public
+#git co master
+#rm -r ./*
+#git co src -- public
+#mv public/* .
+#rm -r public
